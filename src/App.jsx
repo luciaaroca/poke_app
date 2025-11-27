7fa23b1cd1fbd91820ce280d573547d440fc8cd6
luciaaroca <lucia.aroca2510@gmail.com>
2025-11-27 21:14:06 +0100

    try/form

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,29 @@
-// import { useContext, useState } from 'react'
+import { useContext, useState } from 'react'
 import { BrowserRouter } from 'react-router-dom'
-// import { UserContext } from './context/UserContext.js'
+import { PokemonContext } from './context/PokemonContext.js' //PROVEEDOR DE CONTEXTO
 
 import './App.css'
 import Header from './components/Header/Header.jsx'
 import Main from './components/Main/Main.jsx'
 import Footer from './components/Footer/Footer.jsx'
 function App() {
-   //Proveer este estado a los compomentes consumidores
-  // const [username, setUsername] = useState('Lucía');
+  //Proveer este estado a los compomentes consumidores
+  const [pokemonList, setPokemonList] = useState([]);
 
   
-  // const updateUsername = (newUsername) => {
-  //   setUsername(newUsername);
-  // };
+  const updatePokemon  = (newPokemon) => {
+    setPokemonList([...pokemonList, newPokemon]);
+  };
   //Datos para alimentar al contexto que se va a proveer
-  // const pokemonData = {pokemonName, updatepokemonName}
+  const pokemonData = {pokemonList, updatePokemon}
   return (
     <>
-    {/* <PokemonContext.Provider value={pokemonData}> */}
-      <BrowserRouter>
-          <Header/>
-          <Main/>
-      </BrowserRouter>
-    {/* </PokemonContext.Provider> */}
+     <PokemonContext.Provider value={pokemonData}> 
+        <BrowserRouter>
+            <Header/>
+            <Main/>
+        </BrowserRouter>
+     </PokemonContext.Provider> 
       <Footer/>
     </>
   )
--- src/components/Main/PokemonForm/PokemonForm.jsx
+++ src/components/Main/PokemonForm/PokemonForm.jsx
@@ -1,8 +1,76 @@
 import React from "react";
+import { useContext, useState} from 'react'
+import { PokemonContext } from '../../../context/PokemonContext'//CONTEXTO
 import "./PokemonForm.css"
-
+//CONSUMIDOR: consumirá este contexto para poder añadir un nuevo pokemon al sistema.
 const PokemonForm = () => {
-  return <div>PokemonForm</div>;
+  //Consumir el CONTEXTO
+  const { updatePokemon } = useContext(PokemonContext);
+  //ESTADOS
+  const [newPokemon, setNewPokemon] = useState({ //FORMULARIO (estado inicial)
+            id: "",
+            name: "",
+            image: "",
+            typeOne:"",
+            typeTwo:"",
+          });
+     
+    //Se ejecuta cada vez que el usuario escribe un input
+    const handleChange = (e) => {
+            setNewPokemon({
+            ...newPokemon,
+            [e.target.name]: e.target.value,
+            });
+        };
+
+     //Función que se ejecuta al enviar el formulario
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        updatePokemon (newPokemon);
+        setNewPokemon({
+          id: "",
+          name: "",
+          image: "",
+          typeOne: "",
+          typeTwo: ""
+        });
+  };
+  return <div>
+    <h1>Add your Pokemon!!</h1>
+    <form className="addPokemon" onSubmit={handleSubmit}>
+        <label>ID:</label>
+        <input type="number" name="id" value={newPokemon.id} onChange={handleChange} required /> 
+    
+        <label>Name:</label>
+        <input
+          type="text"
+          name="name"
+          value={newPokemon.name}
+          onChange={handleChange}
+          minLength="3"
+          required
+        />
+        
+        <label>Photo (URL):</label>
+        <input type="text" name="image" value={newPokemon.image} onChange={handleChange} required />
+    
+        {/* <label>Type 1:</label>
+        <select name="typeOne" value={newPokemon.typeOne} onChange={handleChange}>
+          <option value="">Select type</option>
+          {newPokemon.map(t => <option key={t} value={t}>{t}</option>)}
+        </select>
+      
+        <label>Type 2:</label>
+        <select name="typeTwo" value={newPokemon.typeTwo} onChange={handleChange}>
+          <option value="">Select type</option>
+          {newPokemon.map(t => <option key={t} value={t}>{t}</option>)}
+        </select> */}
+       
+      <button type="submit">ADD</button>
+    </form>
+    </div>
 };
+  
+
 
 export default PokemonForm;
